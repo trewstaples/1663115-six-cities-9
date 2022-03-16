2a20b3ee8ac02d94547b2561cbe6dd162882b898
LAlex <trewstaples@gmail.com>
2022-03-16 10:45:44 +0300

    Создаст компонент ReviewsList

--- project/src/pages/offer/offer.tsx
+++ project/src/pages/offer/offer.tsx
@@ -1,6 +1,6 @@
 import Header from '../../components/header/header';
 import { Reviews } from '../../types/offers-types';
-import ReviewsForm from '../reviews-form/reviews-form';
+import ReviewsList from '../reviews-list/reviews-list';
 import { useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { Offers } from '../../types/offers-types';
@@ -89,39 +89,7 @@ function Offer({ isNavigationState: navigationState, offers, reviews }: OfferPro
                   <p className="property__text">{offer.description}</p>
                 </div>
               </div>
-              <section className="property__reviews reviews">
-                <h2 className="reviews__title">
-                  Reviews &middot; <span className="reviews__amount">1</span>
-                </h2>
-                <ul className="reviews__list">
-                  {reviews.map((review, id) => {
-                    const keyValue = `${id}-${review.name}`;
-                    return (
-                      <li key={keyValue} className="reviews__item">
-                        <div className="reviews__user user">
-                          <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                            <img className="reviews__avatar user__avatar" src={review.avatar} width="54" height="54" alt="Reviews avatar" />
-                          </div>
-                          <span className="reviews__user-name">{review.name}</span>
-                        </div>
-                        <div className="reviews__info">
-                          <div className="reviews__rating rating">
-                            <div className="reviews__stars rating__stars">
-                              <span style={{ width: `${Math.round(review.rating) * 20}%` }}></span>
-                              <span className="visually-hidden">Rating</span>
-                            </div>
-                          </div>
-                          <p className="reviews__text">{review.text}</p>
-                          <time className="reviews__time" dateTime={review.date.toString()}>
-                            {review.date.toLocaleString('en', { month: 'long' })} {review.date.toLocaleString('en', { year: 'numeric' })}
-                          </time>
-                        </div>
-                      </li>
-                    );
-                  })}
-                </ul>
-                <ReviewsForm />
-              </section>
+              <ReviewsList reviews={reviews} />
             </div>
           </div>
           <section className="property__map map"></section>
---
+++ project/src/pages/reviews-list/reviews-list.tsx
@@ -0,0 +1,46 @@
+import { Reviews } from '../../types/offers-types';
+import ReviewsForm from '../reviews-form/reviews-form';
+
+type ReviewsListPropsType = {
+  reviews: Reviews;
+};
+
+export function ReviewsList({ reviews }: ReviewsListPropsType): JSX.Element {
+  return (
+    <section className="property__reviews reviews">
+      <h2 className="reviews__title">
+        Reviews &middot; <span className="reviews__amount">1</span>
+      </h2>
+      <ul className="reviews__list">
+        {reviews.map((review, id) => {
+          const keyValue = `${id}-${review.name}`;
+          return (
+            <li key={keyValue} className="reviews__item">
+              <div className="reviews__user user">
+                <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                  <img className="reviews__avatar user__avatar" src={review.avatar} width="54" height="54" alt="Reviews avatar" />
+                </div>
+                <span className="reviews__user-name">{review.name}</span>
+              </div>
+              <div className="reviews__info">
+                <div className="reviews__rating rating">
+                  <div className="reviews__stars rating__stars">
+                    <span style={{ width: `${Math.round(review.rating) * 20}%` }}></span>
+                    <span className="visually-hidden">Rating</span>
+                  </div>
+                </div>
+                <p className="reviews__text">{review.text}</p>
+                <time className="reviews__time" dateTime={review.date.toString()}>
+                  {review.date.toLocaleString('en', { month: 'long' })} {review.date.toLocaleString('en', { year: 'numeric' })}
+                </time>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <ReviewsForm />
+    </section>
+  );
+}
+
+export default ReviewsList;
