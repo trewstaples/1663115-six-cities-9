0fed64d407a7c675a86bbfe01d94296ebe99c94e
LAlex <trewstaples@gmail.com>
2022-03-21 12:49:58 +0300

    Обернёт App в Prodiver

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,10 @@
 import App from './components/app/app';
+import { CITY } from './mocks/city';
+import { offersAmsterdam } from './mocks/offers-mocks';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { offersAmsterdam } from './mocks/offers-mocks';
-import { CITY } from './mocks/city';
+import { store } from './store';
 
 const Setting = {
   PLACES_COUNT: 1000,
@@ -10,7 +12,9 @@ const Setting = {
 
 ReactDOM.render(
   <React.StrictMode>
-    <App placesCount={Setting.PLACES_COUNT} offers={offersAmsterdam} city={CITY} />
+    <Provider store={store}>
+      <App placesCount={Setting.PLACES_COUNT} offers={offersAmsterdam} city={CITY} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
