373ce9d64a4fe02b50858692d4c10fb8ae3b9bb9
LAlex <trewstaples@gmail.com>
2022-03-23 15:18:27 +0300

    Пофиксит названия

--- project/src/pages/offers-sort/offers-sort.tsx
+++ project/src/pages/offers-sort/offers-sort.tsx
@@ -23,7 +23,7 @@ function OffersSort(): JSX.Element {
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
       <span className="places__sorting-type" tabIndex={0} onClick={() => setIsSortOpened(!isSortOpened)}>
-        {activeSortType}
+        {OffersSortType[activeSortType]}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
