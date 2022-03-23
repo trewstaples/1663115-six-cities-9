4bb064f362b5ef04790cbaad8d8ac5b8ac3a5bb6
LAlex <trewstaples@gmail.com>
2022-03-23 13:41:56 +0300

    Настроит открытие/закрытие попапа типа сортировок

--- project/src/pages/offers-sort/offers-sort.tsx
+++ project/src/pages/offers-sort/offers-sort.tsx
@@ -4,29 +4,31 @@ import { offersSortTypes, OffersSortType } from '../../const';
 import { OffersSortTypeKey } from '../../types/offers-sort';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setOffersSortType } from '../../store/action';
+import { useState } from 'react';
 
 //Поставить колбэк каждом уэлементу списка, который будет менять режим active у элементов
 //В колбэк добавить смену сортировки исходного массива
 
 function OffersSort(): JSX.Element {
+  const [isSortOpened, setIsSortOpened] = useState<boolean>(false);
   const activeSortType = useAppSelector((state) => state.offersSortType);
   const dispatch = useAppDispatch();
 
   const handleSortTypeChange = (offersSortType: OffersSortTypeKey) => {
     dispatch(setOffersSortType({ offersSortType }));
+    setIsSortOpened(false);
   };
-  console.log(activeSortType);
 
   return (
     <form className="places__sorting" action="#" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
-        Popular
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setIsSortOpened(!isSortOpened)}>
+        {activeSortType}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened">
+      <ul className={`places__options places__options--custom ${isSortOpened ? 'places__options--opened' : ''}`}>
         {offersSortTypes.map((offersSortType) => (
           <li
             className={`places__option ${activeSortType === offersSortType ? 'places__option--active' : ''}`}
