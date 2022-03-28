28dead7dc57b445e34e1480f510bf6a6492d3c43
LAlex <trewstaples@gmail.com>
2022-03-28 15:07:27 +0300

    Пофиксит карточки предложения

--- project/src/pages/offer-card/offer-card.tsx
+++ project/src/pages/offer-card/offer-card.tsx
@@ -8,12 +8,16 @@ type OfferCardPropsType = {
 function OfferCard({ offer }: OfferCardPropsType): JSX.Element {
   return (
     <>
-      <div className="place-card__mark">
-        <span>{offer.isPremium}</span>
-      </div>
+      {offer.isPremium ? (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      ) : (
+        ''
+      )}
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="/">
-          <img className="place-card__image" src={offer.images[0]} width="260" height="200" alt="Place"></img>
+          <img className="place-card__image" src={offer.previewImage} width="260" height="200" alt="Place"></img>
         </a>
       </div>
       <div className="place-card__info">
@@ -38,7 +42,7 @@ function OfferCard({ offer }: OfferCardPropsType): JSX.Element {
         <h2 className="place-card__name">
           <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
         </h2>
-        <p className="place-card__type">{offer.type}</p>
+        <p className="place-card__type">{offer.type[0].toUpperCase() + offer.type.substring(1)}</p>
       </div>
     </>
   );
