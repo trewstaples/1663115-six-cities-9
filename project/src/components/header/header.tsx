1e446b89c00c77bedd103eae1936332eea8e51c7
LAlex <trewstaples@gmail.com>
2022-03-28 23:04:07 +0300

    Свяжет Header с localStorage по авторизации

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks';
 
 type HeaderPropsType = {
   isNavigationState: boolean;
 };
 
 function Header({ isNavigationState: navigationState }: HeaderPropsType): JSX.Element {
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
   return (
     <header className="header">
       <div className="container">
@@ -18,17 +21,31 @@ function Header({ isNavigationState: navigationState }: HeaderPropsType): JSX.El
           {navigationState ? (
             <nav className="header__nav">
               <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </Link>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
+                {authorizationStatus === AuthorizationStatus.Auth ? (
+                  <>
+                    <li className="header__nav-item user">
+                      <Link to={AppRoute.Favorites} className="header__nav-link header__nav-link--profile">
+                        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                        <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                      </Link>
+                    </li>
+
+                    <li className="header__nav-item">
+                      <a className="header__nav-link" href="/">
+                        <span className="header__signout">Sign out</span>
+                      </a>
+                    </li>
+                  </>
+                ) : (
+                  <li className="header__nav-item user">
+                    <li className="header__nav-item">
+                      <Link to={AppRoute.Login} className="header__nav-link header__nav-link--profile">
+                        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                        <span className="header__login">Sign in</span>
+                      </Link>
+                    </li>
+                  </li>
+                )}
               </ul>
             </nav>
           ) : (
