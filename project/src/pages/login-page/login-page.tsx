7c433ccfa8fe6c8d220bdb8733a855f7371aa44a
LAlex <trewstaples@gmail.com>
2022-04-02 22:52:30 +0300

    Вынесет Header из LoginPage

--- project/src/pages/login-page/login-page.tsx
+++ project/src/pages/login-page/login-page.tsx
@@ -1,6 +1,5 @@
 import { AuthData } from '../../types/auth';
 import { ChangeEvent, FormEvent, useState } from 'react';
-import Header from '../../components/header/header';
 import { loginAction } from '../../store/user/api-actions';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { Navigate, useNavigate } from 'react-router-dom';
@@ -35,7 +34,7 @@ function LoginPage(): JSX.Element {
 
   const isValid = PasswordValidity.PASSWORD_CHARACTER.test(password) && PasswordValidity.PASSWORD_NUMBER.test(password);
 
-  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
     if (login !== null && password !== null) {
@@ -47,37 +46,33 @@ function LoginPage(): JSX.Element {
   };
 
   return (
-    <div className="page page--gray page--login">
-      <Header loginNavState={LOGIN_NAV_STATE} />
-
-      <main className="page__main page__main--login">
-        <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form onSubmit={handleSubmit} className="login__form form" action="#" method="post">
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input onChange={handleLoginChange} className="login__input form__input" type="email" name="email" placeholder="Email" required />
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input onChange={handlePasswordChange} className="login__input form__input" type="password" name="password" placeholder="Password" required />
-              </div>
-              <button className="login__submit form__submit button" type="submit" disabled={!isValid}>
-                Sign in
-              </button>
-            </form>
-          </section>
-          <section className="locations locations--login locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="/">
-                <span>Amsterdam</span>
-              </a>
+    <main className="page__main page__main--login">
+      <div className="page__login-container container">
+        <section className="login">
+          <h1 className="login__title">Sign in</h1>
+          <form onSubmit={handleFormSubmit} className="login__form form" action="#" method="post">
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">E-mail</label>
+              <input onChange={handleLoginChange} className="login__input form__input" type="email" name="email" placeholder="Email" required />
             </div>
-          </section>
-        </div>
-      </main>
-    </div>
+            <div className="login__input-wrapper form__input-wrapper">
+              <label className="visually-hidden">Password</label>
+              <input onChange={handlePasswordChange} className="login__input form__input" type="password" name="password" placeholder="Password" required />
+            </div>
+            <button className="login__submit form__submit button" type="submit" disabled={!isValid}>
+              Sign in
+            </button>
+          </form>
+        </section>
+        <section className="locations locations--login locations--current">
+          <div className="locations__item">
+            <a className="locations__item-link" href="/">
+              <span>Amsterdam</span>
+            </a>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
 
