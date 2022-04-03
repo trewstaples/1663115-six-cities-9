0ec7bf50b696cf9a281a51000689f6a8d09a361f
LAlex <trewstaples@gmail.com>
2022-04-03 22:36:19 +0300

    Уберет баг с NotFoundPage

--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -3,44 +3,19 @@ import { AppRoute } from '../../const';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"></img>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
+    <main className="page__main page__main--index page__main--index-empty">
+      <div className="cities">
+        <div className="cities__places-container container">
+          <section className="cities__places places">
+            <h2 className="visually-hidden">Places</h2>
+            <b className="places__found">
+              <p>404.Страница не найдена</p>
+            </b>
+            <Link to={AppRoute.Main}>Вернуться на главную</Link>
+          </section>
         </div>
-      </header>
-
-      <div className="cities__places-container container">
-        <section className="cities__places places">
-          <h2 className="visually-hidden">Places</h2>
-          <b className="places__found">
-            <p>404.Страница не найдена</p>
-          </b>
-          <Link to={AppRoute.Main}>Вернуться на главную</Link>
-        </section>
       </div>
-    </div>
+    </main>
   );
 }
 
