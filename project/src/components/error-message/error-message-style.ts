4c8782896d5a02150b7ea7421b489a9843f95c14
LAlex <trewstaples@gmail.com>
2022-04-04 23:27:09 +0300

    Заменит enum на стили

--- project/src/components/error-message/error-message-style.ts
+++
@@ -1,9 +0,0 @@
-export const enum ErrorMessageStyle {
-  Position = 'fixed',
-  Top = '30px',
-  Right = '30px',
-  Padding = '10px',
-  BackgroundColor = '#d96666',
-  Color = 'White',
-  BorderRadius = '5px',
-}
--- project/src/components/error-message/error-message.tsx
+++ project/src/components/error-message/error-message.tsx
@@ -1,26 +1,13 @@
 import { getError } from '../../store/user-data/selector';
 import { useAppSelector } from '../../hooks';
-import { ErrorMessageStyle } from './error-message-style';
+
+import './css/error-alert.css';
 
 function ErrorMessage(): JSX.Element | null {
   const error = useAppSelector(getError);
 
   if (error) {
-    return (
-      <div
-        style={{
-          position: `${ErrorMessageStyle.Position}`,
-          top: `${ErrorMessageStyle.Top}`,
-          right: `${ErrorMessageStyle.Right}`,
-          padding: `${ErrorMessageStyle.Padding}`,
-          backgroundColor: `${ErrorMessageStyle.BackgroundColor}`,
-          color: `${ErrorMessageStyle.Color}`,
-          borderRadius: `${ErrorMessageStyle.BorderRadius}`,
-        }}
-      >
-        {error}
-      </div>
-    );
+    return <div className="error-alert">{error}</div>;
   }
 
   return null;
