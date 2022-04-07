1a9a95086d9143d432d8e8993d8098d6c248f2ed
LAlex <trewstaples@gmail.com>
2022-04-08 00:01:44 +0300

    Напишет тесты для NotFoundPage

---
+++ project/src/components/pages/not-found-page/not-found-page.test.tsx
@@ -0,0 +1,22 @@
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import HistoryRouter from '../../history-router/history-router';
+import NotFoundPage from './not-found-page';
+
+describe('Component: NotFoundPage', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <HistoryRouter history={history}>
+        <NotFoundPage />
+      </HistoryRouter>,
+    );
+
+    const headerElement = screen.getByText('404. Page not found');
+    const linkElement = screen.getByText('Вернуться на главную');
+
+    expect(headerElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
+});
