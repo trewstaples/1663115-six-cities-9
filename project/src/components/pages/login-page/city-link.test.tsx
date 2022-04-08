d68a7d20b5ec747d275135e0184c3fb269095877
LAlex <trewstaples@gmail.com>
2022-04-08 13:44:54 +0300

    Напишет тесты для CityLink

---
+++ project/src/components/pages/login-page/city-link.test.tsx
@@ -0,0 +1,19 @@
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import HistoryRouter from '../../history-router/history-router';
+import CityLink from './city-link';
+
+const history = createMemoryHistory();
+const cityTab = 'Paris';
+
+describe('Component: CityLink', () => {
+  it('should render correctly', () => {
+    render(
+      <HistoryRouter history={history}>
+        <CityLink />
+      </HistoryRouter>,
+    );
+
+    expect(screen.getByText(cityTab)).toBeInTheDocument();
+  });
+});
